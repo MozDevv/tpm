79b04e6305fa6ddea2a2a7301f0f8fa42800f0e7
Sammy Karani Mathenge <sammy.mathenge@agilebiz.co.ke>
2024-09-17 11:39:56 +0300

    Deploy to server 1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-//export const BASE_CORE_API = process.env.NEXT_PUBLIC_API_BASE_URL;
-export const BASE_CORE_API = "https://pmistest-api.treasury.go.ke/";
+// export const BASE_CORE_API = process.env.NEXT_PUBLIC_API_BASE_URL;
+//export const BASE_CORE_API = "https://pmistest-api.treasury.go.ke/";
+export const BASE_CORE_API = "https://tntapi.agilebiz.co.ke/";
