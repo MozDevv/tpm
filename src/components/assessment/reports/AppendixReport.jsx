38764d230b86361aa17258148afb5cf360322dfb
Sammy Karani Mathenge <sammy.mathenge@agilebiz.co.ke>
2025-02-05 09:42:19 +0300

    Appendinx report fix

--- src/components/assessment/reports/AppendixReport.jsx
+++ src/components/assessment/reports/AppendixReport.jsx
@@ -150,7 +150,55 @@ const Page5Report = ({ setOpenGratuity, clickedItem }) => {
         generatePdfBlob();
       }, 100); // Adjust the delay as needed
     }
+
+    console.log('this is the clicked item', clickedItem);
   }, [report, pensionableService]);
+
+  /**  "prospectivePensionerAwards": [
+    {
+      "prospective_pensioner_id": "fc3beb7c-f22b-4f48-b91d-d19428fd978c",
+      "pension_award_id": "c85ffa52-1519-464b-9fad-a2b3362cfb2b",
+      "pension_award": {
+        "id": "c85ffa52-1519-464b-9fad-a2b3362cfb2b",
+        "pension_cap_id": "3be49117-74d7-428b-b4b3-25a836109927",
+        "prefix": "APN/PC",
+        "code": 33,
+        "has_commutation": false,
+        "name": "RETIREMENT ON AGE GROUNDS GRATUITY",
+        "description": "Retirement On Age Grounds Gratuity",
+        "start_date": null,
+        "end_date": null
+      },
+      "is_active": true,
+      "id": "0b8f798a-d75f-4469-b211-38892d646b53",
+      "created_by": "1677f2b9-99d3-41f8-ad74-dba8c777180f",
+      "created_date": "2025-02-03T04:41:38.862817Z",
+      "updated_by": null,
+      "updated_date": null
+    },
+    {
+      "prospective_pensioner_id": "fc3beb7c-f22b-4f48-b91d-d19428fd978c",
+      "pension_award_id": "f8cff874-b5fa-4e41-80bc-f9edb6dbbefa",
+      "pension_award": {
+        "id": "f8cff874-b5fa-4e41-80bc-f9edb6dbbefa",
+        "pension_cap_id": "3be49117-74d7-428b-b4b3-25a836109927",
+        "prefix": "APN/PC",
+        "code": 0,
+        "has_commutation": true,
+        "name": "RETIREMENT ON AGE GROUNDS PENSION",
+        "description": "Retirement On Age Grounds Pension",
+        "start_date": null,
+        "end_date": null
+      },
+      "is_active": true,
+      "id": "5eec9557-4504-4a9d-a25d-404925362f99",
+      "created_by": "1677f2b9-99d3-41f8-ad74-dba8c777180f",
+      "created_date": "2025-02-03T04:41:38.862813Z",
+      "updated_by": null,
+      "updated_date": null
+    }
+  ],
+   */
 
   return (
     <div
@@ -257,7 +305,8 @@ const Page5Report = ({ setOpenGratuity, clickedItem }) => {
                 <p>
                   Pension Number:{' '}
                   <strong className="font-normal ml-3">
-                    {clickedItem?.pensioner_number}
+                    {clickedItem?.prospectivePensionerAwards[0].pension_award
+                      ?.prefix + clickedItem?.pensioner_number}
                   </strong>
                 </p>
                 <p>
@@ -266,17 +315,14 @@ const Page5Report = ({ setOpenGratuity, clickedItem }) => {
                     {clickedItem?.mda_description}
                   </strong>
                 </p>
-                {clickedItem?.prospectivePensionerAwards[0] && (
+                {clickedItem?.prospectivePensionerAwards.map((item) => (
                   <p key={clickedItem.prospectivePensionerAwards[0].id}>
                     Claim Type:
                     <strong className="font-normal ml-3">
-                      {
-                        clickedItem.prospectivePensionerAwards[0].pension_award
-                          ?.prefix
-                      }
+                      {item.pension_award?.name}
                     </strong>
                   </p>
-                )}
+                ))}
               </div>
               <div className="font-semibold">
                 <p>
@@ -364,7 +410,7 @@ const Page5Report = ({ setOpenGratuity, clickedItem }) => {
                 <tbody className="text-[13px]">
                   {pensionableService?.map((service) => (
                     <tr key={service.id}>
-                      <td className="py-1 text-gray-700 px-1">
+                      <td className="py-1 text-gray-700 text-[12px] px-1 whitespace-nowrap">
                         {new Date(
                           clickedItem?.date_of_confirmation
                         ).toLocaleDateString('en-GB', {
@@ -373,7 +419,7 @@ const Page5Report = ({ setOpenGratuity, clickedItem }) => {
                           year: 'numeric',
                         })}
                       </td>
-                      <td className="py-1 text-gray-700">
+                      <td className="py-1 text-gray-700 text-[12px] whitespace-nowrap">
                         {new Date(
                           clickedItem?.retirement_date
                         ).toLocaleDateString('en-GB', {
--- src/components/pensionsComponents/preclaims/documents/test.js
+++ src/components/pensionsComponents/preclaims/documents/test.js
@@ -1,188 +1,44 @@
-import { useState } from 'react';
-import {
-  Collapse,
-  Divider,
-  Menu,
-  MenuItem,
-  Button,
-  IconButton,
-  Tooltip,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Checkbox,
-  FormControlLabel,
-} from '@mui/material';
-import { Search, Add, FilterList, SortByAlpha } from '@mui/icons-material';
-
-const FilterComponent = ({ columnDefs, filteredData }) => {
-  const [openFilter, setOpenFilter] = useState(false);
-  const [filters, setFilters] = useState([]); // To store an array of filter objects
-  const [filterType, setFilterType] = useState(2); // Default to 'Includes'
-  const [sortColumn, setSortColumn] = useState('');
-  const [sortCriteria, setSortCriteria] = useState(1); // Default to Ascending
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [selectedColumns, setSelectedColumns] = useState([
-    columnDefs[0]?.field,
-  ]);
-  const [openDialog, setOpenDialog] = useState(false);
-
-  const handleDialogClose = () => setOpenDialog(false);
-  const handleDialogOpen = () => setOpenDialog(true);
-
-  const handleColumnSelect = (col) => {
-    setSelectedColumns((prevSelected) =>
-      prevSelected.includes(col)
-        ? prevSelected.filter((item) => item !== col)
-        : [...prevSelected, col]
-    );
-  };
-
-  const handleApplyFilter = () => {
-    setOpenFilter(true);
-    setOpenDialog(false);
-  };
-
-  const resetFilters = () => {
-    setFilters([]); // Reset all filters
-    setSortColumn('');
-    setSortCriteria(1);
-    setSelectedColumns([columnDefs[0]?.field]);
-  };
-
-  const handleAddFilter = (column, value, type) => {
-    setFilters((prevFilters) => [
-      ...prevFilters,
-      {
-        propertyName: column,
-        propertyValue: value,
-        criterionType: type,
-      },
-    ]);
-  };
-
-  const handleFilters = async () => {
-    const filterParams = filters.reduce((acc, filter, index) => {
-      acc[`filterCriterion.criterions[${index}].propertyName`] =
-        filter.propertyName;
-      acc[`filterCriterion.criterions[${index}].propertyValue`] =
-        filter.propertyValue;
-      acc[`filterCriterion.criterions[${index}].criterionType`] =
-        filter.criterionType || 2; // Default to 'Includes'
-      return acc;
-    }, {});
-
-    console.log('Filter Parameters:', filterParams);
-  };
-
-  return (
-    <div>
-      {/* Column Selection Dialog */}
-      <Dialog
-        open={openDialog}
-        onClose={handleDialogClose}
-        sx={{
-          '& .MuiDialog-paper': {
-            width: '400px',
-            borderRadius: '8px',
-          },
-        }}
-      >
-        <DialogTitle>Select Columns for Filtering</DialogTitle>
-        <DialogContent>
-          {columnDefs.map((col) => (
-            <FormControlLabel
-              key={col.field}
-              control={
-                <Checkbox
-                  checked={selectedColumns.includes(col.field)}
-                  onChange={() => handleColumnSelect(col.field)}
-                />
-              }
-              label={col.headerName}
-            />
-          ))}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleDialogClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleApplyFilter} color="primary">
-            Add Filter(s)
-          </Button>
-        </DialogActions>
-      </Dialog>
-
-      <div className="h-[100%] bg-white w-[300px] rounded-md p-3">
-        <div className="flex w-full justify-between items-center">
-          <p className="text-md font-medium text-primary p-3">Filter By:</p>
-        </div>
-        <Divider sx={{ px: 2 }} />
-
-        {/* Loop over filters */}
-        {filters.map((filter, index) => (
-          <div className="flex flex-col item-center p-4 mt-3" key={index}>
-            <label className="text-xs font-semibold text-gray-600">
-              {filter.propertyName}:
-            </label>
-            <div className="flex">
-              <input
-                type="text"
-                className="border p-2 pl-10 bg-gray-100 border-gray-300 rounded-md text-sm w-[100%]"
-                value={filter.propertyValue}
-                onChange={(e) => {
-                  const newFilters = [...filters];
-                  newFilters[index].propertyValue = e.target.value;
-                  setFilters(newFilters);
-                }}
-                placeholder={`Search ${filter.propertyName}`}
-              />
-            </div>
-          </div>
-        ))}
-
-        <Button onClick={handleDialogOpen} startIcon={<Add />}>
-          Add Filter(s)
-        </Button>
-
-        <Divider sx={{ px: 2 }} />
-
-        {/* Sorting section */}
-        <div className="flex flex-col item-center p-4 mt-3">
-          <label className="text-xs font-semibold text-gray-600 w-[100%]">
-            Sort By:
-          </label>
-          <div className="flex items-center">
-            <select
-              name="role"
-              value={sortColumn}
-              onChange={(e) => setSortColumn(e.target.value)}
-              className="border p-3 bg-gray-100 border-gray-300 rounded-md w-[100%] text-sm"
-            >
-              {columnDefs.map((col) => (
-                <option key={col.field} value={col.field}>
-                  {col.headerName}
-                </option>
-              ))}
-            </select>
-            <Tooltip
-              title={
-                sortCriteria === 1 ? 'Ascending Order' : 'Descending Order'
-              }
-              placement="top"
-            >
-              <IconButton
-                onClick={() => setSortCriteria(sortCriteria === 1 ? 2 : 1)}
-              >
-                <SortByAlpha />
-              </IconButton>
-            </Tooltip>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default FilterComponent;
+// "prospectivePensionerAwards": [
+//   {
+//     "prospective_pensioner_id": "fc3beb7c-f22b-4f48-b91d-d19428fd978c",
+//     "pension_award_id": "c85ffa52-1519-464b-9fad-a2b3362cfb2b",
+//     "pension_award": {
+//       "id": "c85ffa52-1519-464b-9fad-a2b3362cfb2b",
+//       "pension_cap_id": "3be49117-74d7-428b-b4b3-25a836109927",
+//       "prefix": "APN/PC",
+//       "code": 33,
+//       "has_commutation": false,
+//       "name": "RETIREMENT ON AGE GROUNDS GRATUITY",
+//       "description": "Retirement On Age Grounds Gratuity",
+//       "start_date": null,
+//       "end_date": null
+//     },
+//     "is_active": true,
+//     "id": "0b8f798a-d75f-4469-b211-38892d646b53",
+//     "created_by": "1677f2b9-99d3-41f8-ad74-dba8c777180f",
+//     "created_date": "2025-02-03T04:41:38.862817Z",
+//     "updated_by": null,
+//     "updated_date": null
+//   },
+//   {
+//     "prospective_pensioner_id": "fc3beb7c-f22b-4f48-b91d-d19428fd978c",
+//     "pension_award_id": "f8cff874-b5fa-4e41-80bc-f9edb6dbbefa",
+//     "pension_award": {
+//       "id": "f8cff874-b5fa-4e41-80bc-f9edb6dbbefa",
+//       "pension_cap_id": "3be49117-74d7-428b-b4b3-25a836109927",
+//       "prefix": "APN/PC",
+//       "code": 0,
+//       "has_commutation": true,
+//       "name": "RETIREMENT ON AGE GROUNDS PENSION",
+//       "description": "Retirement On Age Grounds Pension",
+//       "start_date": null,
+//       "end_date": null
+//     },
+//     "is_active": true,
+//     "id": "5eec9557-4504-4a9d-a25d-404925362f99",
+//     "created_by": "1677f2b9-99d3-41f8-ad74-dba8c777180f",
+//     "created_date": "2025-02-03T04:41:38.862813Z",
+//     "updated_by": null,
+//     "updated_date": null
+//   }
+// ],
